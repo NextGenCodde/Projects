71229ffd429558faa086ed02b885a46774a16477
Faizan <faizan@example.com>
2025-04-13 20:13:35 +0500

    save changes

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
-function toggleTheme() {
-    document.body.classList.toggle('light');
-  }
+
+
+
+  const toggleBtn = document.getElementById('modeToggle');
+  toggleBtn.addEventListener('click', () => {
+    document.body.classList.toggle('light-mode');
+  });
