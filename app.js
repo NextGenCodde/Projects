26b5ffcc4a0013132a7277f9e816911eb8e3e427
Faizan <faizan@example.com>
2025-04-13 20:35:03 +0500

    save changes

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 
 
 let main = document.querySelector('main')
+let section = document.querySelector('section')
   const toggleBtn = document.getElementById('modeToggle');
   toggleBtn.addEventListener('click', () => {
     document.body.classList.toggle('light-mode');
     main.classList.toggle("bg-change")
+  for( let i = 0 ; i< section.length; i++){
+    section[i].classList.toggle("bg-change")
+  }
   });
